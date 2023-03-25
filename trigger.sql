92c87a6d24d457391ea1e8470dab991e1aedb489
richa <richa21086@iiitd.ac.in>
2023-03-26 01:08:57 +0530

    1 trigger rehta

--- trigger.sql
+++ trigger.sql
@@ -1,23 +1,24 @@
 use final;
-CREATE TRIGGER check_product_status
-BEFORE UPDATE ON PRODUCT
-FOR EACH ROW
-BEGIN
-    IF NEW.status = 'In stock' THEN
-        UPDATE product
-        SET status = 'Out of stock'
-        SET user_id = NEW.user_id
-        WHERE product_id = NEW.product_id;
-    END IF;
-END;
+-- check before adding to cart(limit 1)
+-- CREATE TRIGGER check_product_status_id
+-- BEFORE UPDATE ON PRODUCT
+-- FOR EACH ROW
+-- BEGIN
+--     IF NEW.status_id = 1 THEN
+--         UPDATE product
+--         SET status_id = 3,
+--             user_id = OLD.user_id
+--         WHERE product_id = OLD.product_id;
+--     END IF;
+-- END;
 
-CREATE TRIGGER update_product_status
+CREATE TRIGGER update_product_status_id
 AFTER INSERT ON payments
 FOR EACH ROW
 BEGIN
     UPDATE product
-    SET status = CASE 
-                    WHEN NEW.amount > 0 THEN 'Out of stock'
+    SET status_id = CASE 
+                    WHEN NEW.amount > 0 THEN '2'
                  END
     WHERE id = (SELECT id FROM product WHERE user_id = NEW.user_id LIMIT 1);
 END;
