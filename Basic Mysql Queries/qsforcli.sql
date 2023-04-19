1e31910702326178fd5a74e76a2c6bc8dc06e9a3
richa <richa21086@iiitd.ac.in>
2023-04-19 18:43:31 +0530

    updated qsforcli.sql

--- Basic Mysql Queries/qsforcli.sql
+++ Basic Mysql Queries/qsforcli.sql
@@ -14,7 +14,10 @@ use final;
 -- query to list product id, name, price from product where status_id is 1(product is available)
 -- select product_id, name, price from product where status_id = 1;
 
--- query join sellbuymap and payments to get the transaction details
+-- query join sellbuymap and payments to get the transaction details of a particular user_id
 select payment_id, payment_date, amount, sellbuymap.sellbuy from sellbuymap inner join payments on sellbuymap.sellbuy_id = payments.sellbuy where user_id = 16;
 
+-- query to get the balance of a particular user_id
 select wallet from user where user_id = 1;
+
+-- add product to 
